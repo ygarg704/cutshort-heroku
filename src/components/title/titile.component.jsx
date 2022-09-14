72ffe623c3d2c2f8f41d03f00cad3fbdcf6c20d4
Yash Garg <yash.garg@workz.com>
2022-09-14 21:42:15 +0400

    image added

--- src/components/title/titile.component.jsx
+++ src/components/title/titile.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import { useStyle } from "./title.theme";
+import logo from "./Capture.PNG";
 
 const TitleComponent = () => {
   const classes = useStyle();
@@ -9,7 +10,16 @@ const TitleComponent = () => {
     <>
       <Grid container className={classes.titleCard} direction="column">
         <Grid item sm={12} xs={12}>
-          <h2>Eden</h2>
+          <h2>
+            <span>
+              <img
+                src={logo}
+                alt="logo"
+                style={{ height: "25px", width: "25px", paddingRight: "3px" }}
+              />
+            </span>
+            Eden
+          </h2>
         </Grid>
       </Grid>
     </>
